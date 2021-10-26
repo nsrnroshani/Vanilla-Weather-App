7e74f7c2e5993a882f06f0ab652155765782a580
nsrnroshani <nsrn.roshani@gmail.com>
2021-10-26 10:19:52 +0330

    showed temperature

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  console.log(temperatureElement);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data.name);
+  let cityElement = document.querySelector("#city");
+  console.log(cityElement);
+  cityElement.innerHTML = response.data.name;
 
+  console.log(response.data.weather[0].description);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  console.log(response.data.main.humidity);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  console.log(response.data.wind.speed);
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "f3009e4852fa0a079dab291dabf020c4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
