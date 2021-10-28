2aa5dc0499a908ca6872095a017b38bc2042d646
nsrnroshani <nsrn.roshani@gmail.com>
2021-10-28 10:52:59 +0330

    added forecast html into JS

--- app.js
+++ app.js
@@ -23,6 +23,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img src="https://openweathermap.org/img/wn/03n@2x.png" width="48">
+     <div class="weather-forecast-temperature">
+       <span class="weather-forecast-temperature-max"> 16°</span> <span class="weather-forecast-temperature-min"> 10°</span>
+     </div>
+    </div>
+    
+  
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
@@ -83,7 +106,7 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
+displayForecast();
 celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
