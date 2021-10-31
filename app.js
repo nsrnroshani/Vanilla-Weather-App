556affa3cabb7750a34ed45f11a5ee7d04cd23e7
nsrnroshani <nsrn.roshani@gmail.com>
2021-10-31 09:51:27 +0330

    added API integration

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function formatDate(timestamp) {
 
   let days = [
     "Sunday",
-    "Mondaye",
+    "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
@@ -22,26 +22,37 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="https://openweathermap.org/img/wn/03n@2x.png" width="48">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" width="48">
      <div class="weather-forecast-temperature">
-       <span class="weather-forecast-temperature-max"> 16°</span> <span class="weather-forecast-temperature-min"> 10°</span>
+       <span class="weather-forecast-temperature-max"> ${Math.round(
+         forecastDay.temp.max
+       )}°</span> <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temp.min
+        )}°</span>
      </div>
-    </div>
-    
-  
-`;
+    </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
